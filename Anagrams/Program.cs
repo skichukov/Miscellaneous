7c1b989a708b87a3c3911b9e78d6345d34ff120e
skichukov <99436299+skichukov@users.noreply.github.com>
2025-05-29 20:39:44 +0300

    Добавена е директория Anagrams
    Тук е задачата, при която има дума и списък от думи и за всички думи от списъка се проверява дали са анаграми на думата.

---
+++ Anagrams/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Anagrams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string W = Console.ReadLine();
+            int N = int.Parse(Console.ReadLine());
+            string[] words = new string[N];
+            bool isAnagram;
+
+            for (int i = 0; i < N; i++)
+            {
+                words[i] = Console.ReadLine();
+            }
+
+            foreach (string word in words)
+            {
+                string sub_W = W;
+
+                if(word == W || word.Length != W.Length)
+                {
+                    Console.WriteLine("No");
+                    continue;
+                }
+
+                isAnagram = true;
+
+                for(int i = 0; i < word.Length; i++)
+                {
+                    if (!sub_W.Contains(word.ToCharArray()[i]))
+                    {
+                        isAnagram = false;
+                        break;
+                    }
+
+                    sub_W = sub_W.Remove(sub_W.IndexOf(word.ToCharArray()[i]), 1);
+                }
+
+                if (!isAnagram)
+                {
+                    Console.WriteLine("No");
+                } else
+                {
+                    Console.WriteLine("Yes");
+                }
+            }
+        }
+    }
+}
