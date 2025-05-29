c537457563c41ae8164887ed204cf66c81581158
skichukov <99436299+skichukov@users.noreply.github.com>
2025-05-29 14:48:30 +0300

    Качен е файлът AloneNumbers

---
+++ AloneNumbers/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace AloneNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int trg = int.Parse(Console.ReadLine());
+            string[] numbers = s.Split(',');
+            string changed = "[" + numbers[0] + ", ";
+
+            for (int i = 1; i < numbers.Length - 1; i++)
+            {
+
+                if (int.Parse(numbers[i]) != trg)
+                {
+                    changed += numbers[i] + ", ";
+                    continue;
+                }
+
+                if (numbers[i - 1] == numbers[i]
+                    || numbers[i] == numbers[i + 1])
+                {
+                    changed += numbers[i] + ", ";
+                    continue;
+                }
+
+                if (int.Parse(numbers[i - 1]) >
+                    int.Parse(numbers[i + 1]))
+                {
+                    numbers[i] = numbers[i - 1];
+                }
+                else
+                {
+                    numbers[i] = numbers[i + 1];
+                }
+
+                changed += numbers[i] + ", ";
+            }
+
+            changed += numbers[numbers.Length - 1] + "]";
+            Console.WriteLine(changed);
+        }
+    }
+}
