42343512ed51918ae5d5c16891ed82d20c491e2a
skichukov <99436299+skichukov@users.noreply.github.com>
2025-05-31 19:50:09 +0300

    Добавяне на поддиректорията CrookedDigits

---
+++ CrookedDigits/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace CrookedDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long sum = 0;
+            Console.OutputEncoding = Encoding.UTF8;
+            string N = Console.ReadLine();
+
+            N = N.Replace("-", "");
+            N = N.Replace(".", "");
+
+            while (N.Length > 1)
+            {
+                for (int i = 0; i < N.Length; i++)
+                {
+                    sum += long.Parse(N.ToCharArray()[i].ToString());
+                }
+
+                N = sum.ToString();
+                sum = 0;
+            }
+
+            Console.WriteLine(N);
+        }
+    }
+}
